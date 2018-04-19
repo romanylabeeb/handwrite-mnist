73a03734bec0061c8aadd692fb60abee6f8a344c
romany_labeeb <rosaber.ext@orange.com>
2018-04-19 10:01:27 +0000

    add hand-write mnist code

---
+++ example/mnist_client_image.py
@@ -0,0 +1,95 @@
+# Copyright 2016 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+#!/usr/bin/env python2.7
+
+"""Send JPEG image to tensorflow_model_server loaded with inception model.
+"""
+
+from __future__ import print_function
+
+# This is a placeholder for a Google-internal import.
+
+from grpc.beta import implementations
+import tensorflow as tf
+from PIL import Image, ImageFilter
+import numpy as np
+from tensorflow_serving.apis import predict_pb2
+from tensorflow_serving.apis import prediction_service_pb2
+import cv2
+from protobuf_to_dict import protobuf_to_dict
+
+
+tf.app.flags.DEFINE_string('server', 'localhost:9000',
+                           'PredictionService host:port')
+tf.app.flags.DEFINE_string('image', '', 'path to image in JPEG format')
+FLAGS = tf.app.flags.FLAGS
+
+
+def imageprepare(argv):
+    im = Image.open(argv).convert('L')
+    width = float(im.size[0])
+    height = float(im.size[1])
+    newImage = Image.new('L', (28, 28), (255))  # creates white canvas of 28x28 pixels
+
+    if width > height:  # check which dimension is bigger
+        # Width is bigger. Width becomes 20 pixels.
+        nheight = int(round((20.0 / width * height), 0))  # resize height according to ratio width
+        if (nheight == 0):  # rare case but minimum is 1 pixel
+            nheight = 1
+            # resize and sharpen
+        img = im.resize((20, nheight), Image.ANTIALIAS).filter(ImageFilter.SHARPEN)
+        wtop = int(round(((28 - nheight) / 2), 0))  # calculate horizontal position
+        newImage.paste(img, (4, wtop))  # paste resized image on white canvas
+    else:
+        # Height is bigger. Heigth becomes 20 pixels.
+        nwidth = int(round((20.0 / height * width), 0))  # resize width according to ratio height
+        if (nwidth == 0):  # rare case but minimum is 1 pixel
+            nwidth = 1
+            # resize and sharpen
+        img = im.resize((nwidth, 20), Image.ANTIALIAS).filter(ImageFilter.SHARPEN)
+        wleft = int(round(((28 - nwidth) / 2), 0))  # caculate vertical pozition
+        newImage.paste(img, (wleft, 4))  # paste resized image on white canvas
+
+    # newImage.save("sample.png
+
+    tv = list(newImage.getdata())  # get pixel values
+
+    # normalize pixels to 0 and 1. 0 is pure white, 1 is pure black.
+    tva = [(255 - x) * 1.0 / 255.0 for x in tv]
+    return tva
+
+def main(_):
+  host, port = FLAGS.server.split(':')
+  channel = implementations.insecure_channel(host, int(port))
+  stub = prediction_service_pb2.beta_create_PredictionService_stub(channel)
+  # Send request
+  with open(FLAGS.image, 'rb') as f:
+    # See prediction_service.proto for gRPC request/response details.
+   # data=cv2.imread(FLAGS.image)
+    x=[imageprepare(FLAGS.image)]
+    data=np.array(x[0])
+    request = predict_pb2.PredictRequest()
+    request.model_spec.name = 'mnist'
+    request.model_spec.signature_name = 'predict_images' 
+    request.inputs['images'].CopyFrom(
+          tf.contrib.util.make_tensor_proto(data,shape=[1, data.size],dtype=tf.float32 ))
+    result = stub.Predict(request, 10.0)  # 10 secs timeou
+    for x in dict(result.outputs)['scores'].float_val :
+       print(x)
+
+
+if __name__ == '__main__':
+  tf.app.run()
